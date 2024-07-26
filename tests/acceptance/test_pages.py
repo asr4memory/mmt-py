3601a424a6db566e9b58959a75b1d73322ae4b12
Marc Altmann <marcaltmann@posteo.de>
2024-07-26 21:44:00 +0200

    WIP tests

--- tests/acceptance/test_pages.py
+++ tests/acceptance/test_pages.py
@@ -1,31 +1,44 @@
 import re
 from playwright.sync_api import Page, expect
 
+BASE_URL = "http://localhost:5173/"
+
 
 def test_has_title(page: Page):
-    page.goto("http://localhost:5173/")
+    page.goto(BASE_URL)
     expect(page).to_have_title(re.compile("MMT"))
 
 
 def test_legal_notice_link(page: Page):
-    page.goto("http://localhost:5173/")
+    page.goto(BASE_URL)
     page.get_by_role("link", name="Impressum").click()
     expect(page.get_by_role("heading", name="Impressum")).to_be_visible()
 
 
 def test_privacy_link(page: Page):
-    page.goto("http://localhost:5173/")
+    page.goto(BASE_URL)
     page.get_by_role("link", name="Datenschutz").click()
     expect(page.get_by_role("heading", name="Datenschutz")).to_be_visible()
 
 
 def test_accessibility_link(page: Page):
-    page.goto("http://localhost:5173/")
+    page.goto(BASE_URL)
     page.get_by_role("link", name="Barrierefreiheit").click()
     expect(page.get_by_role("heading", name="Barrierefreiheit")).to_be_visible()
 
 
 def test_contact_link(page: Page):
-    page.goto("http://localhost:5173/")
+    page.goto(BASE_URL)
     page.get_by_role("link", name="Kontakt").click()
     expect(page.get_by_role("heading", name="Kontakt")).to_be_visible()
+
+
+def test_login(page: Page):
+    page.goto(BASE_URL)
+    page.get_by_role("link", name="Anmelden").click()
+    page.get_by_label('Benutzername').fill('test')
+    page.get_by_label('Passwort').fill('test')
+    page.get_by_role("button", name="Anmelden").click()
+
+    # TODO: For this we need another test setup!
+    # page.get_by_role("link", name="test").click()
