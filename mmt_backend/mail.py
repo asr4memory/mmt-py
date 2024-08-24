f679dc686d18898289b093a4729b8ff56ed3a0d0
Marc Altmann <marcaltmann@posteo.de>
2024-08-24 14:16:42 +0200

    Improve emails

--- mmt_backend/mail.py
+++ mmt_backend/mail.py
@@ -4,6 +4,9 @@ from flask_mail import Mail, Message
 mail = Mail()
 
 
+CONST_SUBJECT_PREFIX = "[mmt-py]"
+
+
 def send_test_email():
     body = render_template("mail/en/test_mail.txt")
     msg = Message(
@@ -15,29 +18,30 @@ def send_test_email():
 
 
 def send_new_user_email(recipients: list[str], user: str):
+    subject = "A new user has registered."
     body = render_template("mail/en/new_user.txt", username=user)
-    msg = Message(
-        "[mmt-py] A new user has registered.", recipients=recipients, body=body
-    )
+    msg = Message(subject=f"{CONST_SUBJECT_PREFIX} {subject}", recipients=recipients, body=body)
     mail.send(msg)
 
 
 def send_admin_file_uploaded_email(recipients: list[str], username: str, filename: str):
+    subject = "File uploaded"
     body = render_template(
         "mail/en/file_uploaded_admin.txt", username=username, filename=filename
     )
-    msg = Message("[mmt-py] File uploaded", recipients=recipients, body=body)
+    msg = Message(subject=f"{CONST_SUBJECT_PREFIX} {subject}", recipients=recipients, body=body)
     mail.send(msg)
 
 
 def send_user_file_uploaded_email(
     to_username: str, to_email: str, filename: str, locale: str = "en"
 ):
+    subject = "Datei hochgeladen" if locale == "de" else "File uploaded"
     body = render_template(
         f"mail/{locale}/file_uploaded_user.txt", username=to_username, filename=filename
     )
     msg = Message(
-        "[mmt-py] File uploaded",
+        subject=f"{CONST_SUBJECT_PREFIX} {subject}",
         recipients=[to_email],
         body=body,
     )
@@ -45,9 +49,14 @@ def send_user_file_uploaded_email(
 
 
 def send_user_activation_email(to_username: str, to_email: str, locale: str = "en"):
+    subject = (
+        "Ihr Konto wurde aktiviert."
+        if locale == "de"
+        else "Your account has been activated."
+    )
     body = render_template(f"mail/{locale}/user_activated.txt", username=to_username)
     msg = Message(
-        "[mmt-py] Your account has been activated.",
+        subject=f"{CONST_SUBJECT_PREFIX} {subject}",
         recipients=[to_email],
         body=body,
     )
