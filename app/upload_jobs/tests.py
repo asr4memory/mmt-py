6acf5a96c933ce70c00e24ce85919bc96523cd98
Marc Altmann <marcaltmann@posteo.de>
2025-03-04 14:25:52 +0100

    Write first test

--- app/upload_jobs/tests.py
+++ app/upload_jobs/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from upload_jobs.models import UploadJob
+
+
+User = get_user_model()
+
+
+class UploadJobTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="alice", password="password")
+        UploadJob.objects.create(title="Test upload job", user=user)
+
+    def test_upload_jobs_can(self):
+        """Directory names are created correctly"""
+        job = UploadJob.objects.get(title="Test upload job")
+
+        actual = job.directory_name()
+        expected = "Test upload job"
+
+        len_expected = len(expected)
+        len_date = 18
+
+        self.assertEqual(actual[:len_expected], expected)
+        self.assertEqual(len(actual), len_expected + 1 + len_date)
