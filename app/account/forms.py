6bf735ac78f9968f82f836f253f9fe3bbbea287e
Marc Altmann <marcaltmann@posteo.de>
2025-03-07 09:42:32 +0100

    Work on custom validator

--- app/account/forms.py
+++ app/account/forms.py
@@ -1,10 +1,34 @@
-from django.forms import ModelForm, RadioSelect
-from django.contrib.auth.forms import BaseUserCreationForm, UsernameField
+import re
+
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import BaseUserCreationForm, UsernameField
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, RadioSelect
+from django.utils.translation import gettext_lazy as _
 
 from .models import Profile
 
 User = get_user_model()
+
+username_format = re.compile(r"^[a-z]+_[a-z0-9]+$")
+
+
+def validate_username(value):
+    if len(value) < 4:
+        raise ValidationError(
+            _("%(value)s is too short"),
+            params={"value": value},
+        )
+    if (len(value) > 12):
+        raise ValidationError(
+            _("%(value)s is too long"),
+            params={"value": value},
+        )
+    if not username_format.match(value):
+        raise ValidationError(
+            _("%(value)s does not have the right format"),
+            params={"value": value},
+        )
 
 
 class RegisterForm(BaseUserCreationForm):
