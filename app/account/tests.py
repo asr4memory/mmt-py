51c6fbda76c3126c410899e405c9eb6bf4a3844d
Marc Altmann <marcaltmann@posteo.de>
2025-03-07 15:56:33 +0100

    Improve test

--- app/account/tests.py
+++ app/account/tests.py
@@ -7,20 +7,24 @@ from account.forms import validate_username
 class ValidateUsernameTestCase(TestCase):
     def test_too_long(self):
         """Raises if username is too long"""
-        self.assertRaises(
-            ValidationError, validate_username, "hello_this_is_a_nice_username"
+        self.assertRaisesMessage(
+            ValidationError,
+            "hello_this_is_a_nice_username is too long",
+            validate_username,
+            "hello_this_is_a_nice_username",
         )
 
     def test_too_short(self):
         """Raises if username is too short"""
-        self.assertRaises(ValidationError, validate_username, "mac")
+        self.assertRaisesMessage(ValidationError, "mac is too short", validate_username, "mac")
 
     def test_incorrect_format(self):
         """Raises if username has an incorrect format"""
-        self.assertRaises(ValidationError, validate_username, "max headroom")
-        self.assertRaises(ValidationError, validate_username, "Max Headroom")
-        self.assertRaises(ValidationError, validate_username, "max2 headroom")
-        self.assertRaises(ValidationError, validate_username, "max headroom.")
+        message = "does not have the right format"
+        self.assertRaisesMessage(ValidationError, message, validate_username, "max headroom")
+        self.assertRaisesMessage(ValidationError, message, validate_username, "Max Headroom")
+        self.assertRaisesMessage(ValidationError, message, validate_username, "max2 head")
+        self.assertRaisesMessage(ValidationError, message, validate_username, "max head.")
 
     def test_correct_format(self):
         """Does not raise if username has correct format"""
