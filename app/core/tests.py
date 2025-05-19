0faa7b1fde06ed8fbc3cab3658b07c1c198be342
Marc Altmann <marcaltmann@posteo.de>
2025-05-19 16:58:21 +0200

    Test primary menu and welcome page

--- app/core/tests.py
+++ app/core/tests.py
@@ -2,6 +2,15 @@ from django.test import TestCase
 
 
 class CoreTests(TestCase):
+    def test_primary_menu(self):
+        """Primary menu shows the right amount of links when logged out."""
+        response = self.client.get("/", follow=True)
+
+        self.assertContains(response, "Downloads")
+        self.assertContains(response, "Log in")
+        self.assertContains(response, "Register")
+
+
     def test_welcome_page(self):
         """Welcome page works"""
         response = self.client.get("/", follow=True)
@@ -9,3 +18,4 @@ class CoreTests(TestCase):
         self.assertContains(
             response, "<h1>Administration software for media files</h1>", html=True
         )
+        self.assertContains(response, "Downloadable files")
