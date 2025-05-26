7533276af08207214414484cf60677240feacfde
Marc Altmann <marcaltmann@posteo.de>
2025-05-26 11:50:34 +0200

    Refactor tests

--- app/mmt/core/tests.py
+++ app/mmt/core/tests.py
@@ -8,16 +8,6 @@ from django.urls import reverse
 User = get_user_model()
 
 
-class LogoutTests(TestCase):
-    def test_log_out_clears_all_site_data(self):
-        """clear site data is set when logging out."""
-        alice = User.objects.create_user(username="alice", password="password")
-        self.client.login(username="alice", password="password")
-
-        response = self.client.post(reverse("logout"), follow=True)
-        self.assertContains(response, "Log in")
-
-
 class CoreTests(TestCase):
     def test_primary_menu(self):
         """Primary menu shows the right amount of links when logged out."""
@@ -27,10 +17,25 @@ class CoreTests(TestCase):
         self.assertContains(response, "Log in")
         self.assertContains(response, "Register")
 
+    def test_welcome_page(self):
+        """Welcome page works"""
+        response = self.client.get(reverse("welcome"))
+
+        self.assertContains(
+            response, "<h1>Administration software for media files</h1>", html=True
+        )
+        self.assertContains(response, "Downloadable files")
+
+
+class CoreLoggedInTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        cls.alice = User.objects.create_user(username="alice", password="password")
+
     def test_fail_fast_for_missing_download_dir(self):
         """Fails fast if download directory is missing"""
-        user = User.objects.create_user(username="alice", password="password")
-        user.destroy_user_directories()
+        self.alice.destroy_user_directories()
         self.client.login(username="alice", password="password")
 
         response = self.client.get(reverse("welcome"))
@@ -42,19 +47,16 @@ class CoreTests(TestCase):
 
     def test_primary_menu_logged_in(self):
         """Primary menu shows the right amount of links when logged in."""
-        alice = User.objects.create_user(username="alice", password="password")
-        alice.create_user_directories()
+        self.alice.create_user_directories()
         self.client.login(username="alice", password="password")
 
         response = self.client.get(reverse("welcome"))
 
         self.assertContains(response, "Log out")
 
-    def test_welcome_page(self):
-        """Welcome page works"""
-        response = self.client.get(reverse("welcome"))
+    def test_log_out_clears_all_site_data(self):
+        """clear site data is set when logging out."""
+        self.client.login(username="alice", password="password")
 
-        self.assertContains(
-            response, "<h1>Administration software for media files</h1>", html=True
-        )
-        self.assertContains(response, "Downloadable files")
+        response = self.client.post(reverse("logout"), follow=True)
+        self.assertContains(response, "Log in")
