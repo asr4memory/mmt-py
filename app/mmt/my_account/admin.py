a601fac1c1a2f10c8d89b40498ee3736dcd427fc
Marc Altmann <marcaltmann@posteo.de>
2025-06-17 11:38:49 +0200

    Add upload jobs link to user admin

--- app/mmt/my_account/admin.py
+++ app/mmt/my_account/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
+from mmt.upload_jobs.models import UploadJob
 from .models import User, Profile
 
 
@@ -12,12 +15,30 @@ class ProfileInline(admin.StackedInline):
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    def uploadjob_link(self, obj):
+        count = UploadJob.objects.filter(user=obj).count()
+        url = (
+            reverse("admin:upload_jobs_uploadjob_changelist")
+            + f"?user__id__exact={obj.id}"
+        )
+        return format_html('<a href="{}">{} ({})</a>', url, _("View upload jobs"), count)
+
+    uploadjob_link.short_description = _("Upload jobs")
+
+    readonly_fields = UserAdmin.readonly_fields + ('uploadjob_link',)
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Related Data'), {
+            'fields': ('uploadjob_link',),
+        }),
+    )
+
     list_display = [
         "username",
         "email",
         "is_active",
         "profile__full_name",
         "profile__locale",
+        "uploadjob_link",
     ]
     inlines = [ProfileInline]
     actions = ["make_active"]
