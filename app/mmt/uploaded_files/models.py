ec9ae7e6fdc520e013697e70ff572ae06b83b409
Marc Altmann <marcaltmann@posteo.de>
2025-06-30 11:43:56 +0200

    Add new fields to UploadedFile model

--- app/mmt/uploaded_files/models.py
+++ app/mmt/uploaded_files/models.py
@@ -3,6 +3,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UploadedFile(models.Model):
+    class UploadStatus(models.TextChoices):
+        CREATED = "created", _("Created")  # Metadata exists, no file yet
+        UPLOADING = "uploading", _("Uploading")  # File is being uploaded
+        COMPLETE = "complete", _("Complete")  # Upload finished successfully
+        CORRUPT = "corrupt", _("Corrupt")  # File uploaded but failed integrity checks
+        MISSING = "missing", _("Missing")  # No file found on disk
+
     upload_job = models.ForeignKey(
         "upload_jobs.UploadJob",
         on_delete=models.CASCADE,
@@ -11,6 +18,16 @@ class UploadedFile(models.Model):
     )
     filename = models.CharField(max_length=255, verbose_name=_("Filename"))
     size = models.BigIntegerField(default=0, verbose_name=_("Size"))
+    chunk_count = models.IntegerField(default=0, verbose_name=_("Chunk count"))
+    chunks_transferred = models.IntegerField(
+        default=0, verbose_name=_("Chunks transferred")
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=UploadStatus.choices,
+        default=UploadStatus.CREATED,
+        verbose_name=_("Status"),
+    )
     transferred = models.BigIntegerField(default=0, verbose_name=_("Transferred"))
     media_type = models.CharField(
         max_length=255, blank=True, null=False, verbose_name=_("Media type")
