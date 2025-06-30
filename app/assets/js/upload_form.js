6c4a8f269b7aa5bb72e7386c0acda5aa312773c7
Marc Altmann <marcaltmann@posteo.de>
2025-06-30 17:05:29 +0200

    Format assets with biome

--- app/assets/js/upload_form.js
+++ app/assets/js/upload_form.js
@@ -1,4 +1,4 @@
-import 'vite/modulepreload-polyfill';
+import "vite/modulepreload-polyfill";
 import { createApp } from "vue/dist/vue.esm-bundler";
 
 import UploadQueueStarter from "./components/upload_queue_starter.js";
