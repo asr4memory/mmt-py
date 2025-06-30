f4ee2867e24db4855891e8d7e03e3a496e277351
Marc Altmann <marcaltmann@posteo.de>
2025-06-30 14:22:29 +0200

    Move some tests over to pytest

--- app/mmt/my_account/tests.py
+++ app/mmt/my_account/tests.py
@@ -8,40 +8,6 @@ from .validators import validate_username
 User = get_user_model()
 
 
-class ValidateUsernameTestCase(TestCase):
-    def test_too_long(self):
-        """Raises if username is too long"""
-        self.assertRaisesMessage(
-            ValidationError,
-            "hello_this_is_a_nice_username_but_it_is_too_long is too long",
-            validate_username,
-            "hello_this_is_a_nice_username_but_it_is_too_long",
-        )
-
-    def test_too_short(self):
-        """Raises if username is too short"""
-        self.assertRaisesMessage(
-            ValidationError, "mac is too short", validate_username, "mac"
-        )
-
-    def test_incorrect_format(self):
-        """Raises if username has an incorrect format"""
-        message = "does not have the right format"
-        self.assertRaisesMessage(
-            ValidationError, message, validate_username, "max headroom"
-        )
-        self.assertRaisesMessage(
-            ValidationError, message, validate_username, "MaxHeadroom"
-        )
-        self.assertRaisesMessage(
-            ValidationError, message, validate_username, "max#!head"
-        )
-
-    def test_correct_format(self):
-        """Does not raise if username has correct format"""
-        self.assertIsNone(validate_username("alice-henderson"))
-
-
 class ProfileTests(TestCase):
     @classmethod
     def setUpTestData(cls):
--- app/mmt/uploaded_files/migrations/0006_alter_uploadedfile_status.py
+++ app/mmt/uploaded_files/migrations/0006_alter_uploadedfile_status.py
@@ -4,15 +4,25 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('uploaded_files', '0005_uploadedfile_chunk_count_and_more'),
+        ("uploaded_files", "0005_uploadedfile_chunk_count_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='uploadedfile',
-            name='status',
-            field=models.CharField(choices=[('created', 'Created'), ('uploading', 'Uploading'), ('complete', 'Complete'), ('corrupt', 'Corrupt'), ('missing', 'Missing')], default='created', max_length=20, verbose_name='Status'),
+            model_name="uploadedfile",
+            name="status",
+            field=models.CharField(
+                choices=[
+                    ("created", "Created"),
+                    ("uploading", "Uploading"),
+                    ("complete", "Complete"),
+                    ("corrupt", "Corrupt"),
+                    ("missing", "Missing"),
+                ],
+                default="created",
+                max_length=20,
+                verbose_name="Status",
+            ),
         ),
     ]
