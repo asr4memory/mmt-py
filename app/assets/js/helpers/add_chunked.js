6e0c02e3fe507ea4f7db5e630ed5cd24469d516c
Marc Altmann <marcaltmann@posteo.de>
2025-07-01 09:27:54 +0200

    Reformat JS and CSS

--- app/assets/js/helpers/add_chunked.js
+++ app/assets/js/helpers/add_chunked.js
@@ -5,27 +5,23 @@ const csrftoken = getCookie("csrftoken");
 const locale = document.documentElement.lang;
 
 export default async function sendChunk(uploadedFileId, file, chunkNumber) {
-  const uploadChunkEndPoint = `/${locale}/uploaded-files/${uploadedFileId}/upload-chunk/${chunkNumber}/`;
+    const uploadChunkEndPoint = `/${locale}/uploaded-files/${uploadedFileId}/upload-chunk/${chunkNumber}/`;
 
-  const chunk = file.slice(
-    chunkNumber * CHUNK_SIZE,
-    (chunkNumber + 1) * CHUNK_SIZE,
-    file.type,
-  );
+    const chunk = file.slice(chunkNumber * CHUNK_SIZE, (chunkNumber + 1) * CHUNK_SIZE, file.type);
 
-  const response = await fetch(uploadChunkEndPoint, {
-    method: "POST",
-    headers: {
-      "X-CSRFToken": csrftoken,
-    },
-    credentials: "include",
-    body: chunk,
-  });
+    const response = await fetch(uploadChunkEndPoint, {
+        method: "POST",
+        headers: {
+            "X-CSRFToken": csrftoken,
+        },
+        credentials: "include",
+        body: chunk,
+    });
 
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`);
-  }
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
 
-  const json = await response.json();
-  return json;
+    const json = await response.json();
+    return json;
 }
