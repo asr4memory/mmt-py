45172a68bb1831652cd6b94125880900bc15e419
Marc Altmann <marcaltmann@posteo.de>
2025-07-01 18:09:16 +0200

    Work on queue

--- app/assets/js/components/upload_queue.js
+++ app/assets/js/components/upload_queue.js
@@ -1,11 +1,10 @@
-import addFile from "../helpers/add_file.js";
 import createChecksum from "../helpers/create_checksum.js";
+import CurrentUpload from "./current_upload.js";
 import FileStorage from "../helpers/file_storage.js";
 import getNextFileId from "../helpers/get_next_file_id.js";
 import registerUpload from "../helpers/register_upload.js";
+import sendFile from "../helpers/send_file.js";
 import submitChecksum from "../helpers/submit_checksum.js";
-import sendFile from '../helpers/send_file.js';
-import CurrentUpload from "./current_upload.js";
 import UploadQueueItem from "./upload_queue_item.js";
 
 let xhrRef = null;
@@ -20,6 +19,7 @@ export default {
     data() {
         const files = this.files || [];
         return {
+            activeJob: null,
             pending: files.map((file) => {
                 const id = getNextFileId();
                 storedFiles.storeFile(id, file);
@@ -29,32 +29,86 @@ export default {
                     filesize: file.size,
                 };
             }),
-            active: null,
         };
     },
-    mounted() {
-        this.startNextJob();
+    async mounted() {
+        while (this.queueHasItems) {
+            await this.processQueue();
+        }
     },
     computed: {
         itemCount() {
-            let result = this.pending.length;
-            if (this.active) {
-                result += 1;
-            }
-            return result;
+            const queueLength = this.pending.length;
+            return this.activeJob ? queueLength + 1 : queueLength;
         },
         isEmpty() {
             return this.itemCount === 0;
         },
+        queueHasItems() {
+            return this.pending.length > 0;
+        }
     },
     methods: {
+        redirectToUploadJobDetailPage() {
+            /* Redirect and leave Vue.js app. */
+            this.$nextTick(() => {
+                window.location.href = `/${this.$i18n.locale}/upload-jobs/${this.uploadJobId}/`;
+            });
+        },
+        async prepareNextJob() {
+            const nextJob = this.pending[0];
+            const nextJobId = nextJob.jobId;
+            const nextJobFile = storedFiles.getFile(nextJobId);
+
+            const registeredUpload = await registerUpload(nextJobFile, this.uploadJobId);
+            if (!registeredUpload) {
+                // Something did not work during upload registration
+                return;
+            }
+
+            const result = {
+                jobId: nextJobId,
+                serverId: registeredUpload.id,
+                serverFilename: registeredUpload.filename,
+                chunkCount: registeredUpload.chunk_count,
+                chunksTransferred: registeredUpload.chunks_transferred,
+                nextChunk: registeredUpload.next_chunk,
+                file: nextJobFile,
+                filesize: nextJobFile.size,
+                transferred: 0,
+                checksumProgress: 0,
+                startedAt: new Date(),
+            };
+            return result;
+        },
+        dequeue() {
+            this.pending = this.pending.slice(1);
+        },
+        async calculateChecksum(file) {
+            const checksum = await createChecksum(file, (progress) => {
+                if (this.activeJob) {
+                    this.activeJob = {
+                        ...this.activeJob,
+                        checksumProgress: progress,
+                    };
+                }
+            });
+            // Set checksum progress to 100% after checksum is calculated.
+            if (this.activeJob) {
+                this.activeJob = {
+                    ...this.activeJob,
+                    checksumProgress: 1,
+                };
+            }
+            return checksum;
+        },
         removeActive() {
-            const activeJob = this.active;
+            const activeJob = this.activeJob;
             xhrRef.abort();
             if (activeJob) {
                 storedFiles.removeFile(activeJob.jobId);
             }
-            this.active = null;
+            this.activeJob = null;
             this.startNextJob();
         },
         removeItem(idToRemove) {
@@ -66,75 +120,21 @@ export default {
             const lastPart = this.pending.slice(index + 1);
             this.pending = firstPart.concat(lastPart);
         },
-        async startNextJob() {
-            if (this.pending.length === 0) {
-                /*
-                 * Queue is empty, job done.
-                 * This is where we leave the Vue.js app.
-                 * Redirect to upload job detail page.
-                 * Waiting for 1 second to allow other requests to finish.
-                 */
-                setTimeout(() => {
-                    window.location.href = `/${this.$i18n.locale}/upload-jobs/${this.uploadJobId}/`;
-                }, 1000);
-                return;
-            }
+        async processQueue() {
+            this.activeJob = await this.prepareNextJob();
+            this.dequeue();
 
-            if (this.active || !this.uploadJobId) {
-                /* This should never be reached. */
-                return;
-            }
+            console.log(this.activeJob);
+            const checksum = await this.calculateChecksum(this.activeJob.file);
+            await submitChecksum(this.activeJob.serverId, checksum);
+            await sendFile(this.activeJob.serverId, this.activeJob.file);
 
-            const nextJob = this.pending[0];
-            const nextJobId = nextJob.jobId;
-            const nextJobFile = storedFiles.getFile(nextJobId);
-
-            const registeredUpload = await registerUpload(nextJobFile, this.uploadJobId);
-            if (!registeredUpload) {
-                // Something did not work during upload registration
-                return;
-            }
-
-            const registeredJob = {
-                jobId: nextJobId,
-                serverId: registeredUpload.id,
-                serverFilename: registeredUpload.filename,
-                chunkCount: registeredUpload.chunk_count,
-                chunksTransferred: registeredUpload.chunks_transferred,
-                nextChunk: registeredUpload.next_chunk,
-                filesize: nextJobFile.size,
-                transferred: 0,
-                checksumProgress: 0,
-                startedAt: new Date(),
-            };
-
-            this.active = registeredJob;
-            this.pending = this.pending.slice(1);
-
-            sendFile(registeredJob.serverId, nextJobFile);
-
-            const checksum = await createChecksum(nextJobFile, (progress) => {
-                if (this.active) {
-                    this.active = {
-                        ...this.active,
-                        checksumProgress: progress,
-                    };
-                }
-            });
-            // Set checksum progress to 100% after checksum is calculated.
-            if (this.active) {
-                this.active = {
-                    ...this.active,
-                    checksumProgress: 1,
-                };
-            }
-
-            const result = await submitChecksum(registeredJob.serverId, checksum);
+            this.activeJob = null;
         },
     },
     template: `
     <ul class="queue u-ll u-mt">
-      <CurrentUpload v-if="active" :upload="active" @onCancelActive="removeActive" />
+      <CurrentUpload v-if="activeJob" :upload="activeJob" @onCancelActive="removeActive" />
       <UploadQueueItem v-for="job in pending" :key="job.jobId" :upload="job" @onCancel="removeItem" />
     </ul>
   `,
