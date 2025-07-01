1c6b9fb3c21465d79a57e4d838d834edfdf692ee
Marc Altmann <marcaltmann@posteo.de>
2025-07-01 11:24:15 +0200

    Fix uploading

--- app/mmt/uploaded_files/urls.py
+++ app/mmt/uploaded_files/urls.py
@@ -6,7 +6,7 @@ app_name = "uploaded_files"
 
 urlpatterns = [
     path("<int:pk>/upload/", views.upload, name="upload"),
-    path("<int:pk>/upload-chunk/<int:chunk>", views.upload_chunk, name="upload-chunk"),
+    path("<int:pk>/upload-chunk/<int:chunk>/", views.upload_chunk, name="upload-chunk"),
     path("<int:pk>/update/", views.update, name="update"),
     path("<int:pk>/delete/", views.delete, name="delete"),
 ]
