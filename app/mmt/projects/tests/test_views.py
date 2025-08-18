51cbb3cdb9365597129c0233ab423c0232a61b8b
Marc Altmann <marcaltmann@posteo.de>
2025-08-18 21:41:09 +0200

    Write 'negative' tests for project detail page

--- app/mmt/projects/tests/test_views.py
+++ app/mmt/projects/tests/test_views.py
@@ -1,14 +1,32 @@
+from http.client import FOUND
+
 from bs4 import BeautifulSoup
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import TestCase
 
 from mmt.projects.models import Project
 
+User = get_user_model()
+
 
 class ProjectViewTests(TestCase):
-    fixtures = ["test_data.json"]
+    @classmethod
+    def setUpTestData(cls):
+        cls.alice = User.objects.create_superuser(
+            username="alice", password="password", email="alice@example.com"
+        )
+        cls.bob = User.objects.create_user(
+            username="bob", password="password", email="bob@example.com"
+        )
+        cls.project = Project.objects.create(user=cls.alice, name="Test project")
 
-    def test_primary_menu_logged_in(self):
-        """Project detail page works."""
+        perm = Permission.objects.get(codename="view_project")
+        cls.bob.user_permissions.add(perm)
+
+
+    def test_project_detail_page(self):
+        """Project detail page renders correctly."""
         self.client.login(username="alice", password="password")
         project = Project.objects.first()
 
@@ -17,3 +35,17 @@ class ProjectViewTests(TestCase):
         project_name = soup.find(attrs={"data-testid": "project-name"})
 
         self.assertIn("Test project", project_name.get_text())
+
+    def test_project_page_logged_out(self):
+        """Redirects if user is not logged in."""
+        project = Project.objects.first()
+        response = self.client.get(f"/projects/{project.id}/")
+        self.assertEqual(response.status_code, FOUND)
+
+    def test_project_detail_page_another_user(self):
+        """Project detail page of another user is not visible."""
+        self.client.login(username="bob", password="password")
+        project = Project.objects.first()
+
+        response = self.client.get(f"/projects/{project.id}/")
+        self.assertEqual(response.status_code, 404)
