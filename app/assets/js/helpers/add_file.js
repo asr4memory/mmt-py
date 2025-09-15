12699158a01f11541d7dfb84a32fcccf54004b45
Marc Altmann <marcaltmann@posteo.de>
2025-09-15 10:42:25 +0200

    Refactor addFile function

--- app/assets/js/helpers/add_file.js
+++ app/assets/js/helpers/add_file.js
@@ -1,41 +1,32 @@
-import getCookie from "./get_cookie.js";
+import getCookie from './get_cookie.js';
 
-const csrftoken = getCookie(document.cookie, "csrftoken");
+const csrftoken = getCookie(document.cookie, 'csrftoken');
 
 export default function addFile(options) {
     const { fileId, file, filename, onProgress, onEnd, onAbort } = options;
+    console.assert(typeof onProgress === 'function');
+    console.assert(typeof onEnd === 'function');
+    console.assert(typeof onAbort === 'function');
 
     const uploadEndPoint = `/uploaded-files/${fileId}/upload/`;
+    const request = buildRequest(uploadEndPoint, onProgress, onEnd, onAbort);
+    const formData = new FormData();
+    formData.append('file', file, filename);
+    request.send(formData);
+    return request;
+}
 
+function buildRequest(url, onProgress, onEnd, onAbort) {
     const request = new XMLHttpRequest();
     request.withCredentials = true;
-
-    request.addEventListener("loadend", () => {
-        if (typeof onEnd === "function") {
-            onEnd();
-        }
-    });
-
-    request.addEventListener("abort", () => {
-        if (typeof onAbort === "function") {
-            onAbort();
-        }
-    });
-
-    request.open("POST", uploadEndPoint);
-    request.setRequestHeader("X-CSRFToken", csrftoken);
-
-    const formData = new FormData();
-    formData.append("file", file, filename);
-
-    const uploadObject = request.upload;
-    uploadObject.addEventListener("progress", (event) => {
-        if (event.lengthComputable && typeof onProgress === "function") {
+    request.open('POST', url);
+    request.setRequestHeader('X-CSRFToken', csrftoken);
+    request.addEventListener('loadend', () => onEnd());
+    request.addEventListener('abort', () => onAbort());
+    request.upload.addEventListener('progress', (event) => {
+        if (event.lengthComputable && typeof onProgress === 'function') {
             onProgress(event.loaded);
         }
     });
-
-    request.send(formData);
-
     return request;
 }
