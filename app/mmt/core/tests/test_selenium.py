aa845261bba0ed523132d4e1fd4bd55ff0e20dc5
Marc Altmann <marcaltmann@posteo.de>
2025-10-14 18:18:24 +0200

    Fix test

--- app/mmt/core/tests/test_selenium.py
+++ app/mmt/core/tests/test_selenium.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium.webdriver.common.by import By
@@ -36,7 +35,7 @@ class CoreSeleniumTests(StaticLiveServerTestCase):
         """Visit welcome page."""
         self.selenium.get(f"{self.live_server_url}/")
         el = self.selenium.find_element(By.TAG_NAME, "h1")
-        self.assertEqual(f"Media Management Tool {settings.MMT_APP_VERSION}", el.text)
+        self.assertEqual(f"Media Management Tool", el.text)
 
     def test_sign_in(self):
         """Test sign in procedure."""
