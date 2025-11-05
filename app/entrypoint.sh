9ea699271d740171eea5e1a11fc46901892718e7
Marc Altmann <marcaltmann@posteo.de>
2025-11-05 15:10:59 +0100

    Reduce verbosity for compilemessages command

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,7 +3,7 @@
 . /app/bin/activate
 cd /app/django-app
 python manage.py check
-python manage.py compilemessages
+python manage.py compilemessages --verbosity 0
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py creategroups
