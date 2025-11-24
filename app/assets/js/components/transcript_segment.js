28b0aad70026052a0df9fe226124152c5a12139c
Marc Altmann <marcaltmann@posteo.de>
2025-11-24 18:35:41 +0100

    WIP

--- app/assets/js/components/transcript_segment.js
+++ app/assets/js/components/transcript_segment.js
@@ -22,10 +22,12 @@ export default {
     <div class="u-mt-small">
         <p>Segment {{index}}; Start: {{startTimecode}}, End: {{endTimecode}}</p>
         <p class="segment u-ll">
+            <TranscriptWord word=" " :score="0" />
             <TranscriptWord v-for="(word, index) in words" :key="word.start"
                 :index="index"
                 :start="word.start" :end="word.end" :word="word.word"
                 :speaker="word.speaker" :score="word.score" />
+            <TranscriptWord word=" " :score="0" />
         </p>
     </div>
     `,
