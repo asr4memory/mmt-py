85d8e13c22ffee56c15db705e5ecb7ef5497574c
Marc Altmann <marcaltmann@posteo.de>
2025-12-02 17:42:42 +0100

    Make Notice content fields rich text fields

--- app/mmt/core/models.py
+++ app/mmt/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from tinymce.models import HTMLField
 
 
 class Notice(models.Model):
@@ -10,12 +11,8 @@ class Notice(models.Model):
     )
     title_en = models.CharField(max_length=255, verbose_name=_("Title (English)"))
     title_de = models.CharField(max_length=255, verbose_name=_("Title (German)"))
-    content_en = models.TextField(
-        blank=True, default="", verbose_name=_("Content (English)")
-    )
-    content_de = models.TextField(
-        blank=True, default="", verbose_name=_("Content (German)")
-    )
+    content_en = HTMLField(blank=True, default="", verbose_name=_("Content (English)"))
+    content_de = HTMLField(blank=True, default="", verbose_name=_("Content (German)"))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created at"))
 
     class Meta:
