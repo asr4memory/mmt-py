16fc3faa1e5c4226f228a4f3c7e893a7198a52a0
Marc Altmann <marcaltmann@posteo.de>
2026-01-07 17:00:33 +0100

    Change temporary upload directory

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,11 @@
 
 . /app/bin/activate
 cd /app/django-app
+
+# Make upload temp directory if it does not exist.
+mkdir -p user_files/_tmp
+
+# Prepare Django
 python manage.py check
 python manage.py compilemessages --verbosity 0
 python manage.py collectstatic --noinput
--- app/mmt/settings.py
+++ app/mmt/settings.py
@@ -335,3 +335,5 @@ MMT_APP_VERSION = get_project_version()
 MMT_USER_FILES_DIR = Path(env('USER_FILES_DIR', default=BASE_DIR / 'user_files'))
 MMT_DETECT_DOWNLOADABLE_FILES = False
 MMT_EMAIL_SUBJECT_PREFIX = '[mmt]'
+
+FILE_UPLOAD_TEMP_DIR = MMT_USER_FILES_DIR / '_tmp'
