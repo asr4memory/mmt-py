9535626c014d942b0109b105ba15e31e60a3ec67
Marc Altmann <marcaltmann@posteo.de>
2026-02-09 22:18:19 +0100

    Add milliseconds to timestamps

--- app/assets/js/helpers/format_timecode.js
+++ app/assets/js/helpers/format_timecode.js
@@ -1,7 +1,9 @@
 export default function formatTimecode(d) {
     const hours = Math.floor(d / 3600);
     const minutes = Math.floor((d % 3600) / 60);
-    const seconds = Math.floor(d % 60);
+    const seconds = d % 60;
+    const roundedSeconds = Math.floor(seconds);
+    const millisecondsStr = seconds.toFixed(3).split('.')[1];
 
-    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${roundedSeconds.toString().padStart(2, "0")}.${millisecondsStr}`;
 }
