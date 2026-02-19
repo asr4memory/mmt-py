9d09a8d44544a97b90916e3125cdc9259521eaa3
Marc Altmann <marcaltmann@posteo.de>
2026-02-19 19:57:48 +0100

    Transcript: Use long-form timecode tick format

--- app/assets/js/components/waveform_component.js
+++ app/assets/js/components/waveform_component.js
@@ -75,17 +75,9 @@ export default {
                 .domain([0, this.waveform.waveform_max])
                 .range([0, 100]);
 
-            const xAxis = d3.axisBottom(xScale).tickFormat((d) => {
-                const hours = Math.floor(d / 3600);
-                const minutes = Math.floor((d % 3600) / 60);
-                const seconds = Math.floor(d % 60);
-
-                if (hours > 0) {
-                    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-                } else {
-                    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
-                }
-            });
+            const xAxis = d3
+                .axisBottom(xScale)
+                .tickFormat((d) => formatTimecode(d));
 
             // This is simpler, but not efficient.
             d3.select("#waveform").selectAll("svg").remove();
