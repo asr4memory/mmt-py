09e84d0601594d5615e4208e9ee952e4514ebea9
Marc Altmann <marcaltmann@posteo.de>
2026-04-09 18:37:15 +0200

    Add TermsRedirectMiddleware

--- app/mmt/my_account/middleware.py
+++ app/mmt/my_account/middleware.py
@@ -1,4 +1,11 @@
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.utils import translation
+
+whitelisted_paths = [
+    reverse('account:accept_terms'),
+    reverse('account_logout'),
+]
 
 
 class AccountLocaleMiddleware:
@@ -21,3 +28,27 @@ class AccountLocaleMiddleware:
         # the view is called.
 
         return response
+
+
+class TermsRedirectMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+        user = request.user
+
+        if (
+            user.is_authenticated
+            and not user.has_accepted_terms
+            and request.path not in whitelisted_paths
+        ):
+            return redirect(reverse('account:accept_terms'))
+        else:
+            return response
