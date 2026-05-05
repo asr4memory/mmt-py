2452298225228a063bfa1a766bb5e8e518db3a8e
Marc Altmann <marcaltmann@posteo.de>
2026-05-05 11:24:52 +0200

    Add upload activity message to Admin dashboard

--- app/mmt/uploaded_files/templatetags/uploaded_files_extras.py
+++ app/mmt/uploaded_files/templatetags/uploaded_files_extras.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django import template
+from django.utils import timezone
 
 from mmt.core.utils import format_duration
 
@@ -9,3 +12,11 @@ register = template.Library()
 def duration(value: float) -> str:
     """Django template filter that formats media duration."""
     return format_duration(value)
+
+
+@register.simple_tag
+def recent_upload_activity() -> bool:
+    from mmt.uploaded_files.models import UploadedFile
+
+    since = timezone.now() - timedelta(minutes=5)
+    return UploadedFile.objects.filter(has_file=False, updated_at__gte=since).exists()
