478c6784c8d69535c48f18efa667b1c7382f8fc3
Marc Altmann <marcaltmann@posteo.de>
2026-05-08 11:30:27 +0200

    Revert "Try to display Django test results in Github"
    This reverts commit 256c67b8d3916983b5a6f9f784dc88a8c2979159.

--- app/mmt/tests/runner.py
+++ app/mmt/tests/runner.py
@@ -1,4 +1,3 @@
-import os
 import warnings
 
 from django.conf import settings
@@ -14,16 +13,6 @@ class MMTTestRunner(DiscoverRunner):
 
         with override_settings(**TEST_SETTINGS):
             return super().run_tests(*args, **kwargs)
-
-    def run_suite(self, suite, **kwargs):
-        if os.environ.get('CI'):
-            import xmlrunner
-            os.makedirs('test-results', exist_ok=True)
-            return xmlrunner.XMLTestRunner(
-                output='test-results',
-                verbosity=self.verbosity,
-            ).run(suite)
-        return super().run_suite(suite, **kwargs)
 
 
 TEST_SETTINGS = {
