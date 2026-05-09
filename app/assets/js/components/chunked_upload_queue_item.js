a5b4d36b16157d01edb6407b7bcd2d5dac62ae3b
Marc Altmann <marcaltmann@posteo.de>
2026-05-09 17:40:12 +0200

    Improve styling of queue item component

--- app/assets/js/components/chunked_upload_queue_item.js
+++ app/assets/js/components/chunked_upload_queue_item.js
@@ -19,28 +19,30 @@ export default {
         sizeStr() {
             return formatBytes(this.upload.file.size, this.$i18n.locale);
         },
+        transferredStr() {
+            return formatBytes(this.upload.file.size * this.upload.progress, this.$i18n.locale);
+        },
         isCancellable() {
             return CANCELLABLE.includes(this.upload.status);
         },
     },
     template: `
     <li :class="['chunked-queue-item', 'chunked-queue-item--' + upload.status]">
+      <progress class="chunked-queue-item__progress" :value="upload.progress" max="1"></progress>
       <div class="chunked-queue-item__body">
         <h3 class="chunked-queue-item__name">{{ upload.file.name }}</h3>
-        <p class="chunked-queue-item__details">{{ sizeStr }} – {{ $t('queue.' + upload.status) }} – Progress: {{ upload.progress }}</p>
+        <p class="chunked-queue-item__details">{{ transferredStr }} / {{ sizeStr }} – {{ $t('queue.' + upload.status) }}</p>
       </div>
-      <div class="chunked-queue-item__actions">
-        <button
-          v-if="isCancellable"
-          type="button"
-          class="chunked-queue-item__button icon-button"
-          :aria-label="$t('cancel')"
-          :title="$t('cancel')"
-          @click="$emit('onCancel', upload)"
-        >
-          <CloseIcon class="chunked-queue-item__icon icon-button__icon" />
-        </button>
-      </div>
+      <button
+        v-if="isCancellable"
+        type="button"
+        class="chunked-queue-item__button"
+        :aria-label="$t('cancel')"
+        :title="$t('cancel')"
+        @click="$emit('onCancel', upload)"
+      >
+        <CloseIcon class="chunked-queue-item__icon icon-button__icon" />
+      </button>
     </li>
   `,
 };
