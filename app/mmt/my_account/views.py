3423d8b904b21be411bda5497ae099d8f17ffd5f
Marc Altmann <marcaltmann@posteo.de>
2026-05-11 21:37:24 +0200

    Clean up dpa_sample view

--- app/mmt/my_account/views.py
+++ app/mmt/my_account/views.py
@@ -123,18 +123,9 @@ def accept_terms(request):
 
 @require_GET
 def dpa_sample(request):
-    accepted_at_str = '[Datum, Uhrzeit, Zeitzone]'
-
-    dt_berlin = timezone.localtime(
-        request.user.terms_accepted_at, timezone=zoneinfo.ZoneInfo('Europe/Berlin')
-    )
-    formatted = dt_berlin.strftime('%d.%m.%Y %H:%M:%S')
-
-    # accepted_at_str = f'{formatted} (MEZ)'
-
     context = dict(
         full_name='[Vor- und Nachnamen des Nutzenden]',
-        dpa_accepted_at=accepted_at_str,
+        dpa_accepted_at='[Datum, Uhrzeit, Zeitzone]',
     )
 
     return render(request, 'dpa/dpa_sample.html', context)
