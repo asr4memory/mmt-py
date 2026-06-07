9a7fa285e3db822603f169603bff65447344aec5
Marc Altmann <marcaltmann@posteo.de>
2026-06-07 16:59:53 +0200

    Add play button, playback speed select, and repel composition to media player
    - Add play/pause toggle button with SVG icons
    - Add playback speed select (0.7x, 1x, 1.5x, 2x)
    - Clicking the video also toggles play/pause
    - Add repel.css composition (flex space-between)
    - Apply repel to media player toolbar

--- app/assets/js/components/media_player.ts
+++ app/assets/js/components/media_player.ts
@@ -12,9 +12,30 @@ export default defineComponent({
     setup(props, { emit, expose }) {
         const mediaRef = ref<HTMLMediaElement | null>(null);
         const isVideo = computed(() => props.mediaType.startsWith("video"));
+        const isPlaying = ref(false);
+        const playbackRate = ref(1);
+        const PLAYBACK_RATES = [0.7, 1, 1.5, 2];
 
         function onTimeUpdate() {
             if (mediaRef.value) emit("timeupdate", mediaRef.value.currentTime);
+        }
+
+        function togglePlay() {
+            if (!mediaRef.value) return;
+            if (mediaRef.value.paused) {
+                mediaRef.value.play();
+            } else {
+                mediaRef.value.pause();
+            }
+        }
+
+        function onPlayPause() {
+            isPlaying.value = mediaRef.value ? !mediaRef.value.paused : false;
+        }
+
+        function setPlaybackRate(rate: number) {
+            playbackRate.value = rate;
+            if (mediaRef.value) mediaRef.value.playbackRate = rate;
         }
 
         function seekLeft() {
@@ -30,24 +51,55 @@ export default defineComponent({
         }
 
         expose({
-            get mediaElement() { return mediaRef.value; },
+            get mediaElement() {
+                return mediaRef.value;
+            },
         });
 
-        return { mediaRef, isVideo, onTimeUpdate, seekLeft, seekRight };
+        return {
+            mediaRef,
+            isVideo,
+            isPlaying,
+            playbackRate,
+            PLAYBACK_RATES,
+            onTimeUpdate,
+            onPlayPause,
+            togglePlay,
+            setPlaybackRate,
+            seekLeft,
+            seekRight,
+        };
     },
     template: `
-    <div>
-        <video v-if="isVideo" id="media-player" ref="mediaRef" controls
-            width="240" class="transcript__media" @timeupdate="onTimeUpdate">
+    <div class="media-player" :class="isVideo ? 'media-player--video' : 'media-player--audio'">
+        <video v-if="isVideo" id="media-player" class="media-player__element transcript__media" ref="mediaRef"
+            width="240" @timeupdate="onTimeUpdate" @play="onPlayPause" @pause="onPlayPause" @click="togglePlay">
             <source :src="src" :type="mediaType" />
         </video>
-        <audio v-else id="media-player" ref="mediaRef" controls
-            width="240" class="transcript__media" @timeupdate="onTimeUpdate">
+        <audio v-else id="media-player" class="media-player__element transcript__media" ref="mediaRef" controls
+            width="240" @timeupdate="onTimeUpdate" @play="onPlayPause" @pause="onPlayPause">
             <source :src="src" :type="mediaType" />
         </audio>
-        <div>
-            <button type="button" @click="seekLeft">&#9194;</button>
-            <button type="button" @click="seekRight">&#9193;</button>
+        <div class="media-player__toolbar repel">
+            <button type="button" class="media-player__button" @click="togglePlay">
+                <svg v-if="isPlaying" viewBox="0 0 24 24" fill="currentColor">
+                    <rect x="6.4"  y="5" width="3.7" height="14" rx="1.3" />
+                    <rect x="13.9" y="5" width="3.7" height="14" rx="1.3" />
+                </svg>
+                <svg v-else viewBox="0 0 24 24" fill="currentColor"><path d="M7 4.8 L18.6 12 L7 19.2 Z" /></svg>
+            </button>
+
+            <button type="button" class="media-player__button" @click="seekLeft">
+                <svg viewBox="0 0 24 24" fill="currentColor"><path d="M11 5 L4 12 L11 19 Z M19 5 L12 12 L19 19 Z" /></svg>
+            </button>
+
+            <button type="button" class="media-player__button" @click="seekRight">
+                <svg viewBox="0 0 24 24" fill="currentColor"><path d="M5 5 L12 12 L5 19 Z M13 5 L20 12 L13 19 Z" /></svg>
+            </button>
+
+            <select class="media-player__speed" :value="playbackRate" @change="setPlaybackRate(+$event.target.value)">
+                <option v-for="rate in PLAYBACK_RATES" :key="rate" :value="rate">{{ rate }}x</option>
+            </select>
         </div>
     </div>
     `,
