692a4e36b2fa556367590f00f579d06bf7c38791
Marc Altmann <marcaltmann@posteo.de>
2026-06-09 11:13:01 +0200

    Split waveform header into separate spans for id, timecode, and duration

--- app/assets/js/components/waveform_component.ts
+++ app/assets/js/components/waveform_component.ts
@@ -153,7 +153,9 @@ export default defineComponent({
     template: `
     <div class="waveform">
         <header class="waveform__header">
-            <span>#{{formattedID}} {{startTimecode}}–{{endTimecode}} ({{formattedDuration}}s)</span>
+            <span class="waveform__header-id">#{{formattedID}}</span>
+            <span class="waveform__header-timecode">{{startTimecode}}–{{endTimecode}}</span>
+            <span class="waveform__header-duration">({{formattedDuration}}s)</span>
         </header>
         <!-- Set tabindex so that div can be focused and receive key events. -->
         <div id="waveform"
