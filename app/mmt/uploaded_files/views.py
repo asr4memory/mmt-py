664f6f74bfd038a25e4ad60c5c65f959eff3df24
Marc Altmann <marcaltmann@posteo.de>
2026-07-28 18:18:38 +0200

    Defer transcript content on the joined transcription jobs

--- app/mmt/uploaded_files/views.py
+++ app/mmt/uploaded_files/views.py
@@ -40,7 +40,9 @@ def detail(request, pk):
     project = uploaded_file.project
     uploaded_file.update_has_file_field()
     transcripts = uploaded_file.transcripts.defer('content')
-    transcription_jobs = uploaded_file.transcription_jobs.select_related('transcript')
+    transcription_jobs = uploaded_file.transcription_jobs.select_related(
+        'transcript'
+    ).defer('transcript__content')
 
     context = dict(
         uploaded_file=uploaded_file,
